ff6c006277ef11c13c9e28c2a4287cae71bee577
Benjamin Metaut <metautbenjamin@hotmail.fr>
2019-03-28 16:22:14 +0100

    change background

--- src/MyHomeScreen.js
+++ src/MyHomeScreen.js
@@ -1,17 +1,33 @@
 import React, {Component} from 'react';
-import { Button } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default class MyHomeScreen extends React.Component {
     static navigationOptions = {
       drawerLabel: 'Home'
     };
-  
+
     render() {
       return (
+
+        <View
+        style={styles.container}>
+
         <Button
           // onPress={() => this.props.navigation.navigate('Notifications')}
           title="Go to notifications"
         />
+
+        </View>
       );
     }
   }
+
+  const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ff7675',
+  },
+
+});
