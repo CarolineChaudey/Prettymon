52cdc16b32a5debb7665d1a277f89c6b62025a87
Caroline Chaudey <13118474+CarolineChaudey@users.noreply.github.com>
2019-03-29 11:09:31 +0100

    add FlatList with all makeup

--- src/SearchMakeup.js
+++ src/SearchMakeup.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import { View, Text } from 'react-native';
 import MakeupSelectors from './MakeupSelectors';
+import apiData from '../apiData.json';
+import { FlatList } from 'react-native-gesture-handler';
 
 export default class SearchMakeup extends Component {
     constructor(props) {
@@ -9,7 +11,8 @@ export default class SearchMakeup extends Component {
         this.brandChanged = this.brandChanged.bind(this);
         this.state = {
             tag: undefined,
-            brand: undefined
+            brand: undefined,
+            results: undefined
         };
     }
 
@@ -17,12 +20,42 @@ export default class SearchMakeup extends Component {
         this.setState({
             tag: newTag
         });
+        this.searchResults();
     }
 
     brandChanged(newBrand) {
         this.setState({
             brand: newBrand
         });
+        this.searchResults();
+    }
+
+    searchResults() {
+        fetch(apiData.baseUrl)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            this.setState({
+                results: data
+            });
+        })
+        .catch((err) => {
+            this.setState({
+                results: err.message
+            });
+        });
+    }
+
+    renderLine(item) {
+        return (
+            <View style={{ padding: 5 }}>
+                <Text>Brand : {item.brand}</Text>
+                <Text>Name : {item.name}</Text>
+                <Text>Category : {item.category}</Text>
+                <Text>type : {item.product_type}</Text>
+                <Text>price : {item.price} {item.price_sign}</Text>
+            </View>
+        );
     }
 
     render() {
@@ -32,8 +65,14 @@ export default class SearchMakeup extends Component {
                     onTagChange={this.tagChanged}
                     onBrandChange={this.brandChanged}
                 />
-                <Text>{this.state.tag || 'no tag'}</Text>
-                <Text>{this.state.brand || 'no brand'}</Text>
+                {
+                    this.state.results ?
+                    <FlatList
+                        data={this.state.results}
+                        renderItem={({item}) => this.renderLine(item)}
+                    />
+                    : <Text>No results</Text>
+                }
             </View>
         );
     }
